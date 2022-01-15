917ca12435dc904cad87fdf492dfa08c1533ec56
Hmlessard <89013364+Hmlessard@users.noreply.github.com>
2022-01-15 14:44:44 -0600

    update modal type, format phone number array

--- assets/js/breweryubery.js
+++ assets/js/breweryubery.js
@@ -4,30 +4,15 @@ var userFormEl = document.querySelector("#brew-form");
 var cityInputEl = document.querySelector("#city");
 
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+var modal = document.getElementById('id01');
+modal.style.display= "block"
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+//If 
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -69,6 +54,26 @@ var displayBreweries = function(data, searchTerm) {
     for (var i = 0; i < data.length; i++) {
     // format data
     var { name, brewery_type, street, city, state, phone} = data[i];
+    
+    var phoneArray = phone.split('');
+
+    var formatedNumber = [];
+    for(var j = 0; j < phoneArray.length; j++){
+
+      if (j == 2 || j === 5 ){
+          formatedNumber.push(phoneArray[j])
+        formatedNumber.push('-')
+      } else {
+        formatedNumber.push(phoneArray[j])
+      }
+    }
+
+    phone = formatedNumber.join('');
+    
+
+
+  
+
     
     let output = `
     <div class="card column is-one-quarter has-background-warning m-2">
